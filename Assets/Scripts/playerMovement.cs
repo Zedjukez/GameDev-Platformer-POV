b452c098e0d9a84f7cacc4eb19e1603602c8523b
Zedjukez <rubenzylom@gmail.com>
2023-02-06 16:32:41 +0100

    2de commit

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -5,27 +5,87 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public GameObject playerCapsule;
-    public Rigidbody playerBody;
-    public Vector3 rawMovement;
+    public Rigidbody rb;
     [SerializeField] private int speed;
     [SerializeField]private Transform playerEye;
     [SerializeField] private float cameraSensitivity;
     [SerializeField] private float camlimitMin;
     [SerializeField] private float camlimitMax;
+    [SerializeField] private KeyCode jumpKey;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private KeyCode sprintKey;
+    [SerializeField] private int sprintSpeed;
+    [SerializeField] private int currentSpeed;
+    [SerializeField] private float interactRange;
 
     private float camAngle = 0.0f;
+    private void Start()
+    {
+        currentSpeed = speed;
+        Cursor.visible = false;
+        rb = GetComponent<Rigidbody>();
+    }
     private void Update()
     {
-        mouseLook();
-        Movement();
-        transform.Translate(rawMovement * speed * Time.deltaTime);
 
+        sprintCheck();
+        mouseLook();
+        RotateBody();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TryJump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
     }
 
     private void RotateBody()
     {
         float xMouse = Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
         transform.Rotate(Vector3.up * xMouse);
+    }
+
+    private void sprintCheck()
+    {
+        if (Input.GetKey(sprintKey))
+        {
+            currentSpeed = speed + sprintSpeed;
+        }
+
+        if (Input.GetKeyUp(sprintKey))
+        {
+            currentSpeed = speed;
+        }
+    }
+
+    private void TryInterat()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(playerEye.position, playerEye.forward, out hit, interactRange)){
+            IInteractable.
+
+        }
+    }
+    private void TryJump()
+    {
+        if (isGrounded())
+        {
+            Jump(jumpForce);
+        }
+    }
+    private bool isGrounded()
+    {
+        RaycastHit hit;
+        return Physics.Raycast(transform.position, -transform.up, out hit, 1.1f);
+    }
+    private void Jump(float jumpForce)
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        rb.AddForce(transform.up* jumpForce, ForceMode.Impulse);
     }
     void mouseLook()
     {
@@ -35,25 +95,13 @@ public class playerMovement : MonoBehaviour
         camAngle = Mathf.Clamp(camAngle, camlimitMin, camlimitMax);
 
     }
-    void Movement()
+    private void Move()
     {
-        if (Input.GetKeyDown("w"))
-        {
-            rawMovement.z += 2;
-        }
-        if (Input.GetKeyDown("s"))
-        {
-            rawMovement.z += -2;
+        float xDir = Input.GetAxis("Horizontal");
+        float zDir = Input.GetAxis("Vertical");
 
-        }
-        if (Input.GetKeyDown("d"))
-        {
-            rawMovement.x += 2;
-        }
-        if (Input.GetKeyDown("a"))
-        {
-            rawMovement.x += -2;
+        Vector3 dir = transform.right * xDir + transform.forward * zDir;
 
-        }
+        rb.velocity = new Vector3(0, rb.velocity.y, 0) + dir.normalized * currentSpeed;
     }
 }
